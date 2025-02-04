a35d7384b2f1b03f4ca536cf4c29b25db35e0c15
kyawkyawkhaing@abank.com.mm <kyawkyawkhaing@abank.com.mm>
2025-02-05 00:48:38 +0630

    fixed path

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output:"Export",
   reactStrictMode: true,
-};
-module.exports = {
   assetPrefix: process.env.NODE_ENV === 'production' ? '/your-repo-name/' : '',
 };
 
-
-export default nextConfig;
+module.exports = nextConfig;
