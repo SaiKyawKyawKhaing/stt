66c57730d3ebddf1be76d5da554af26215eafbae
kyawkyawkhaing@abank.com.mm <kyawkyawkhaing@abank.com.mm>
2025-02-06 10:13:44 +0630

    change test

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,16 @@
+import { useEffect,useState } from "react";
 import { useFetchJson } from "../utils/util";
 
 export default function Home() {
-  const data = useFetchJson("/index.json");
+  // const data = useFetchJson("/index.json");
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+      fetch("index.json")
+          .then((response) => response.json())
+          .then((result) => setData(result));
+          console.log("DDDD",data)
+  }, []); // Runs only once on mount
   return (
     <div>
       <h1>Users</h1>
